3e138014f5c8774aba3396f109a0072eff03f897
j-m-d-h@users.noreply.github.com <marcxdawson@gmail.com>
2019-12-03 18:01:44 -0500

    Please enter the commit message for your changes. Lines starting

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="lambdata-j-m-d-h",
-    version = "0.1.2",
+    version = "0.1.3",
     author = "j-m-d-h",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
