3c73f6c5b53fdff220efc275c25378b4533d410f
j-m-d-h@users.noreply.github.com <marcxdawson@gmail.com>
2019-12-03 18:52:40 -0500

    fixed trainvaltest bug

--- lambdata_jmdh/test2.py
+++ lambdata_jmdh/test2.py
@@ -2,6 +2,6 @@ from sklearn.model_selection import train_test_split
 
 def train_val_test(df):
     train_val, test = train_test_split(df, test_size=0.1)
-    train, val = train_test_split(train_val, test_size=0.8)
+    train, val = train_test_split(train_val, test_size=0.2)
 
     return train, val, test
